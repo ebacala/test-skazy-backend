0750c080764382b713202e9542a02fc010cb1c83
Evann BACALA <d3v@ebacala.com>
2024-05-10 15:24:44 +1100

    :bug: Fixed CORS issue

--- src/main/java/com/ebacala/controller/HealthController.java
+++ src/main/java/com/ebacala/controller/HealthController.java
@@ -1,9 +1,11 @@
 package com.ebacala.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HealthController {
     @GetMapping("/healthcheck")
     public String getHealthcheck() {
